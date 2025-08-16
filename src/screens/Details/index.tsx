2a68bc0e43ff0f54f4ad76c5199b22bd52855c75
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-08-16 11:55:45 +0800

    Basic & Structure

---
+++ src/screens/Details/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+type Props = {};
+
+const Details = (props: Props) => {
+  return (
+    <View>
+      <Text>Details</Text>
+    </View>
+  );
+};
+
+export default Details;
+
+const styles = StyleSheet.create({});
